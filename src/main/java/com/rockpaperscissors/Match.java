cdc88a88fd90d5b354dfb7e0bfaf6d7dc549d4a8
Mabel Lee <leemabel.711@gmail.com>
2022-09-18 21:02:32 +0200

    refactor code (WIP)

--- src/main/java/com/rockpaperscissors/Match.java
+++ src/main/java/com/rockpaperscissors/Match.java
@@ -1,52 +1,212 @@
 package main.java.com.rockpaperscissors;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Match {
-    private int numberOfMatches;
-    private int maxRockChoice;
-    private int maxPaperChoice;
-    private int maxScissorsChoice;
-    private ArrayList<String> handSignArr = new ArrayList<>();
+    private int numOfMatches;
+    private int numOfAWins;
+    private int numOfDraws;
+    private int numOfBWins;
+    private int aWinsCounter = 0;
+    private int bWinsCounter = 0;
+    private int drawsCounter = 0;
+    private String[] handsignArr = {};
+    private ArrayList<String> handSignArrList = new ArrayList<>();
+    private Random random = new Random();
+    private int randomNum;
+    private String newChoiceB;
+    private ArrayList<String> currentHandsignArrList = new ArrayList<>();
 
-    public ArrayList<String> getHandsignArr() {
-        return this.handSignArr;
+    public String[] getHandsignArr() {
+        return this.handsignArr;
+    }
+    public ArrayList<String> getHandsignArrList() {
+        return this.handSignArrList;
     }
 
     public void setHandsignArr(String[] handSignArr) {
         for(int i = 0; i < handSignArr.length; i++) {
-            this.handSignArr.add(handSignArr[i]);
+            this.handSignArrList.add(handSignArr[i]);
         }
     }
 
     public int getNumOfMatches() {
-        return this.numberOfMatches;
+        return this.numOfMatches;
     }
 
-    public void setNumOfMatches(int numberOfMatches) {
-        this.numberOfMatches = numberOfMatches;
+    public void setNumOfMatches(int numOfMatches) {
+        this.numOfMatches = numOfMatches;
     }
 
-    public int getMaxRockChoice() {
-        return maxRockChoice;
+    public int getNumOfAWin() {
+        return numOfAWins;
     }
 
-    public void setMaxRockChoice(int maxRockChoice) {
-        this.maxRockChoice = maxRockChoice;
+    public void setNumOfAWin(int numOfAWins) {
+        this.numOfAWins = numOfAWins;
     }
 
-    public int getMaxPaperChoice() {
-        return maxPaperChoice;
+    public int getNumOfDraws() {
+        return numOfDraws;
     }
 
-    public void setMaxPaperChoice(int maxPaperChoice) {
-        this.maxPaperChoice = maxPaperChoice;
+    public void setNumOfDraws(int numOfDraws) {
+        this.numOfDraws = numOfDraws;
     }
 
-    public int getMaxScissorsChoice() {
-        return maxScissorsChoice;
+    public int getNumOfBWins() {
+        return numOfBWins;
     }
 
-    public void setMaxScissorsChoice(int maxScissorsChoice) {
-        this.maxScissorsChoice = maxScissorsChoice;
+    public void setNumOfBWins(int numOfBWins) {
+        this.numOfBWins = numOfBWins;
+    }
+
+    // Generate random choice for player B
+    public String selectChoice(String[] arr) {
+        randomNum = random.nextInt(arr.length);
+        // String selectedChoice = handsignArr[randomNum];
+        // String[] temp = getHandsignArr();
+        String selectedChoice = arr[randomNum];
+
+        return selectedChoice;
+    }
+    
+    // Regenerate new random choice for player B
+    public String reselectChoice(ArrayList<String> handsignArrList) {
+        String reselectedChoice = "";
+        if (handsignArrList.size() == 1) {
+            reselectedChoice = handsignArrList.get(0);
+        } else {
+            randomNum = random.nextInt(handsignArrList.size());
+            reselectedChoice = handsignArrList.get(randomNum);
+        }
+
+        return reselectedChoice;
+    }
+
+    // Generate new array list with new options of handsign
+    public ArrayList<String> createHandsignArrList(String oldChoiceB, String[] handsignArr) {
+        this.currentHandsignArrList.clear();
+        for(int i = 0; i < handsignArr.length; i++) {
+            if (!handsignArr[i].equals(oldChoiceB)) {
+                this.currentHandsignArrList.add(handsignArr[i]);
+            }
+        }
+
+        return this.currentHandsignArrList;
+    }
+
+    // Regenerate new array list with new options of handsign after fail choice of first new array list generated
+    public ArrayList<String> createHandsignArrList(String oldChoiceB, ArrayList<String> handsignArrList) {
+        int index = handsignArrList.indexOf(oldChoiceB);
+        handsignArrList.remove(index);
+        this.currentHandsignArrList = handsignArrList;
+
+        return this.currentHandsignArrList;
+    }
+
+    // Recomparing choices selected from new generated array list of handsign options
+    public void recompareChoices(String choiceA, String reselectedChoice) {
+        if(reselectedChoice.equals("rock")) {
+            if(this.aWinsCounter < this.numOfAWins) {
+                this.aWinsCounter++;
+                results(choiceA, reselectedChoice);
+            } else {
+                createHandsignArrList(reselectedChoice, this.currentHandsignArrList);
+                this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                recompareChoices(choiceA, this.newChoiceB);
+            }
+        } else if (newChoiceB.equals("paper")) {
+            if(this.drawsCounter < this.numOfDraws) {
+                this.drawsCounter++;
+                results(choiceA, reselectedChoice);
+            } else {
+                createHandsignArrList(reselectedChoice, this.currentHandsignArrList);
+                this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                recompareChoices(choiceA, this.newChoiceB);
+            }
+        } else if (newChoiceB.equals("scissors")) {
+            if(this.bWinsCounter < this.numOfBWins) {
+                this.bWinsCounter++;
+                results(choiceA, reselectedChoice);
+            } else {
+                createHandsignArrList(reselectedChoice, this.currentHandsignArrList);
+                this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                recompareChoices(choiceA, this.newChoiceB);
+            }
+        }
+    }
+
+    // Print out results of each match
+    public void results(String choiceA, String choiceB) {
+        String strB = "";
+        System.out.println("A's choice: " + choiceA);
+        if(this.newChoiceB.equals("")) {
+            System.out.println("B's choice: " + choiceB);
+            strB = choiceB;
+        } else {
+            System.out.println("B's choice: " + this.newChoiceB);
+            strB = this.newChoiceB;
+        }
+        switch(strB){
+            case "rock":
+                System.out.println("Player A wins!");
+                break;
+            case "paper":
+                System.out.println("It's a tie!");
+                break;
+            case "scissors":
+                System.out.println("Player B wins!");
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
+
+        }
+        System.out.println();
+    }
+
+    // To start the game
+    public void startMatch(int numOfMatches, String choiceA, String choiceB) {
+        for (int i = 1; i <= numOfMatches; i++) {
+            //String choiceB = selectChoice(handsignArr);
+            newChoiceB = "";
+            System.out.println("Round " + i);
+            if(choiceB.equals("rock")) {
+                if(this.aWinsCounter < this.numOfAWins) {
+                    this.aWinsCounter++; 
+                    results(choiceA, choiceB);
+                } else {
+                    this.currentHandsignArrList = createHandsignArrList(choiceB, this.handsignArr);
+                    this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                    recompareChoices(choiceA, this.newChoiceB);
+                }
+            } else if(choiceB.equals(choiceA)) {
+                if(this.drawsCounter < this.numOfDraws) {
+                    this.drawsCounter++;
+                    results(choiceA, choiceB);
+                } else {
+                    this.currentHandsignArrList = createHandsignArrList(choiceB, this.handsignArr);
+                    this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                    recompareChoices(choiceA, this.newChoiceB);
+                }
+            } else if (choiceB.equals("scissors")) {
+                if(this.bWinsCounter < this.numOfBWins) {
+                    this.bWinsCounter++;
+                    results(choiceA, choiceB);
+                } else {
+                    this.currentHandsignArrList = createHandsignArrList(choiceB, this.handsignArr);
+                    this.newChoiceB = reselectChoice(this.currentHandsignArrList);
+                    recompareChoices(choiceA, this.newChoiceB);
+                }
+            }
+        }
+        System.out.println("Below are the total amount of each choices made by player B.");
+        System.out.println("Rock: " + aWinsCounter);    
+        System.out.println("Paper: " + drawsCounter);
+        System.out.println("Scissors: " + bWinsCounter);
+        int total = aWinsCounter + drawsCounter + bWinsCounter;
+        System.out.println("rock + paper + scissors = " + total);
     }
 }
--- src/main/java/com/rockpaperscissors/RockPaperScissors.java
+++ src/main/java/com/rockpaperscissors/RockPaperScissors.java
@@ -3,161 +3,50 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RockPaperScissors {
-    static int numOfRounds = 100;
-    static int maxRocks = 31;
-    static int maxPaper = 32;
-    static int maxScissors = 37;
+    // static int numOfRounds = 100;
+    // static int aWins = 31;
+    // static int maxPaper = 32;
+    // static int maxScissors = 37;
     static int rockCounter = 0;
     static int paperCounter = 0;
     static int scissorsCounter = 0;
-    static String[] handsignArr = {"rock", "paper", "scissors"};
+    //static String[] handsignArr = {"rock", "paper", "scissors"};
     static String choiceA = "paper";
     static String choiceB = "";
     static String newChoiceB = "";
     static ArrayList<String> currentHandsignArrList = new ArrayList<>();
     static Random random = new Random();
     static int randomNum;
-
-    public static String selectChoice(String[] arr) {
-        randomNum = random.nextInt(arr.length);
-        String selectedChoice = handsignArr[randomNum];
-
-        return selectedChoice;
-    }
-
-    public static ArrayList<String> handsignArrList(String oldChoiceB, String[] handsignArray) {
-        currentHandsignArrList.clear();
-        for(int i = 0; i < handsignArr.length; i++) {
-            if (!handsignArr[i].equals(oldChoiceB)) {
-                currentHandsignArrList.add(handsignArr[i]);
-            }
-        }
-
-        return currentHandsignArrList;
-    }
-
-    public static ArrayList<String> handsignArrList(String oldChoiceB, ArrayList<String> handsignArrayList) {
-        int index = handsignArrayList.indexOf(oldChoiceB);
-        handsignArrayList.remove(index);
-        currentHandsignArrList = handsignArrayList;
-
-        return currentHandsignArrList;
-    }
-
-    public static String reselectChoice(ArrayList<String> handsignArrList) {
-        String reselectedChoice;
-        if (handsignArrList.size() > 1) {
-            randomNum = random.nextInt(handsignArrList.size());
-            reselectedChoice = handsignArrList.get(randomNum);
-        } else {
-            reselectedChoice = handsignArrList.get(0);
-        }
-
-        return reselectedChoice;
-    }
-
-    public static void recompareChoices(String choiceA, String reselectedChoice) {
-        if(reselectedChoice.equals("rock")) {
-            if(rockCounter < maxRocks) {
-                rockCounter++;
-                results();
-            } else {
-                handsignArrList(reselectedChoice, currentHandsignArrList);
-                newChoiceB = reselectChoice(currentHandsignArrList);
-                recompareChoices(choiceA, newChoiceB);
-            }
-        } else if (newChoiceB.equals("paper")) {
-            if(paperCounter < maxPaper) {
-                paperCounter++;
-                results();
-            } else {
-                handsignArrList(reselectedChoice, currentHandsignArrList);
-                newChoiceB = reselectChoice(currentHandsignArrList);
-                recompareChoices(choiceA, newChoiceB);
-            }
-        } else if (newChoiceB.equals("scissors")) {
-            if(scissorsCounter < maxScissors) {
-                scissorsCounter++;
-                results();
-            } else {
-                handsignArrList(reselectedChoice, currentHandsignArrList);
-                newChoiceB = reselectChoice(currentHandsignArrList);
-                recompareChoices(choiceA, newChoiceB);
-            }
-        }
-    }
-
-    public static void results() {
-        String strB = "";
-        System.out.println("A's choice: " + choiceA);
-        if(newChoiceB.equals("")) {
-            System.out.println("B's choice: " + choiceB);
-            strB = choiceB;
-        } else {
-            System.out.println("B's choice: " + newChoiceB);
-            strB = newChoiceB;
-        }
-        switch(strB){
-            case "rock":
-                System.out.println("Player A wins!");
-                break;
-            case "paper":
-                System.out.println("It's a tie!");
-                break;
-            case "scissors":
-                System.out.println("Player B wins!");
-                break;
-            default:
-                System.out.println("Invalid choice.");
-                break;
-
-        }
-        System.out.println();
-    }
-
-    public static void startMatch(int numOfRounds) {
-        for (int i = 1; i <= numOfRounds; i++) {
-            choiceB = selectChoice(handsignArr);
-            System.out.println("Round " + i);
-            if(choiceB.equals("rock")) {
-                if(rockCounter < maxRocks) {
-                    rockCounter++; 
-                    results();
-                } else {
-                    currentHandsignArrList = handsignArrList(choiceB, handsignArr);
-                    newChoiceB = reselectChoice(currentHandsignArrList);
-                    recompareChoices(choiceA, newChoiceB);
-                }
-            } else if(choiceB.equals(choiceA)) {
-                if(paperCounter < maxPaper) {
-                    paperCounter++;
-                    results();
-                } else {
-                    currentHandsignArrList = handsignArrList(choiceB, handsignArr);
-                    newChoiceB = reselectChoice(currentHandsignArrList);
-                    recompareChoices(choiceA, newChoiceB);
-                }
-            } else if (choiceB.equals("scissors")) {
-                if(scissorsCounter < maxScissors) {
-                    scissorsCounter++;
-                    results();
-                } else {
-                    currentHandsignArrList = handsignArrList(choiceB, handsignArr);
-                    newChoiceB = reselectChoice(currentHandsignArrList);
-                    recompareChoices(choiceA, newChoiceB);
-                }
-            }
-        }
-        System.out.println("Below are the total amount of each choices made by player B.");
-        System.out.println("Rock: " + rockCounter);    
-        System.out.println("Paper: " + paperCounter);
-        System.out.println("Scissors: " + scissorsCounter);
-        int total = rockCounter + paperCounter + scissorsCounter;
-        System.out.println("rock + paper + scissors = " + total);
-    }
+    
     public static void main(String[] args) throws Exception {
 
-        startMatch(numOfRounds);
+        Player playerA = new Player();
+        Player playerB = new Player();
+        Match rockpaperscissorsMatch = new Match();
+
+        //Assign name to each player
+        playerA.setName("Player A");
+        playerB.setName("Player B");
+
+        //Assign number of matches to be played (For this task: 100)
+        rockpaperscissorsMatch.setNumOfMatches(4);
+
+        //Assign options for handsigns
+        String[] handsignArr = {"rock", "paper", "scissors"};
+        rockpaperscissorsMatch.setHandsignArr(handsignArr);
+
+        //Assign how many times A wins, B win and how many ties
+        rockpaperscissorsMatch.setNumOfAWin(1);
+        rockpaperscissorsMatch.setNumOfBWins(1);
+        rockpaperscissorsMatch.setNumOfDraws(2);
+
+        //Assign choice of each player
+        String choiceA = "paper";
+        String choiceB = rockpaperscissorsMatch.selectChoice(handsignArr);
+
+        // int numOfMatches = rockpaperscissorsMatch.getNumOfMatches();
+
+        rockpaperscissorsMatch.startMatch(rockpaperscissorsMatch.getNumOfMatches(), choiceA, choiceB);
 
     }
 }
